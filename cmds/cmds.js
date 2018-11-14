483c2fc1fff691ece9dd4ccef53000b3a61690aa
Gabe Mertz <gabe.mertz@crms.org>
2018-11-14 18:01:56 +0800

    commented code, changed css/html

--- cmds/cmds.js
+++ cmds/cmds.js
@@ -1,30 +1,35 @@
 var users = require('./users.js');
-var cmds = [//to add, block, print blocked, remove friend
+var cmds = [
+//name, call function, description
 {n:"n",f:rname,d:"/n <name> \n  sets users name to <name>"},
 {n:"add",f:add,d:"/add <name-1> ... <name-n> \n  send users friend request"},
 {n:"msg",f:msg,d:"/msg <name> <message> \n  Send a friend a message"},
-/*{n:"whois",f:users.isUserm,d:"/whois <name> \n  Check who user <name> is"},*/
 {n:"help",f:help,d:"/help \n  You just used this one!"},
 {n:"friends",f:prntFriends,d:"/friends prints all friends"},
 {n:"block",f:block,d:"/block <name-1> ... <name.n> \n  Block some jerks"},
 {n:"rm",f:removeFriend,d:"/rm <name-1> ... <name-n> \n  Un-friend a person"},
-{n:"blocked",f:prntBlocked,d:"/blocked \n  print all people you've blocked"}];
+{n:"blocked",f:prntBlocked,d:"/blocked \n  print all people you've blocked"}
+
+];
 
 /* -----  Exports  -----*/
-exports.run =function(cmd,socketid){//test
+
+//run a given command for user with given socket id
+exports.run =function(cmd,socketid){
 	var args = cmd.split(" ");
 	var cmd = args[0];
 	var res = null;
 	for(var i = 0; i<cmds.length; i++){
 		var c = cmds[i];
-		if(c.n === cmd){
+		if(c.n === cmd)
 			res = c.f(args.slice(1),socketid);
-		}
 	}
 	if(res === null)
 		return resultify("No command named: |" + cmd + "| found!",false,socketid,"server message");
 	return res;
 }
+
+//get changes
 exports.getChanges = function(socketid){
 	var user = users.getUser(socketid);
 	var info = user.newData();
@@ -42,41 +47,49 @@ function help(args,socketid){
 	}
 	return resultify(msg,true,socketid,'server message');
 }
-function printu(args,socketid){//test
+
+//currently not inplemented
+function printu(args,socketid){
 	return resultify("sorry, I killed this cmd.",true,socketid,'server message');
 }
-function rname(args, socketid){//seems to work
+
+//rename a user
+function rname(args, socketid){
 	if(args.length != 1)
-		return resultify("Invalid amount of arguments, name should be a single word, no spaces.", false, socketid, 'server message');
+		return resultify("Invalid amount of arguments, no spaces in names", false, socketid, 'server message');
 	var name = args[0];
 	if(users.isUser(name) !== false)
 		return	resultify("Name already in use!", false, socketid, 'server message');
 	if(validName(name) === false)
-		return resultify("Invalid name! Please use only letters and numbers.", false, socketid, 'server message');
-	console.log(socketid,"Is now: ",name);
+		return resultify("Invalid name! Only letters and numbers", false, socketid, 'server message');
+	//console.log(socketid,"Is now: ",name);
+	
 	var res = users.updateUser(socketid,{name:name});
 	return resultify("Name changed to: "+name, true, socketid, 'server message',{data:res,type:"user data"});
 }
-function add(args, socketid){//wip
+//add friend(s)
+function add(args, socketid){
+
 	var me = users.getUser(socketid);
 	var unadded = [];
+
 	for (let a of args){
 		var friend = users.isUser(a);
 		if(friend !== false && friend !== me){
-			if(me.isFriend(friend) === false){//test, add console.logz if still not work
+			if(me.isFriend(friend) === false){
 				friend.notify("Friend Request",socketid);
 				me.addFriend(friend);
-			}else
-				unadded.push(friend.name+"(already on your friend list)");
+			}else unadded.push(friend.name+"(already on your friend list)");
 		}
-		else
-			unadded.push(a);
+		else unadded.push(a);
 	}
 	if(unadded.length === 0)
 		return resultify("added "+ args.join(', '),true,socketid,'server message');
 	return resultify("Unable to add: " + unadded.join(', ') ,false,socketid,'server message');
 
 }
+
+//message a friend
 function msg(args, socketid){
 	var me = users.getUser(socketid);
 	var receiver = args[0];
@@ -92,15 +105,17 @@ function msg(args, socketid){
 	u.notify('Private Message',me.id,message);
 	return true;
 }
+
 function prntFriends(args,socketid){return resultify(users.getUser(socketid).printFriends(),true,socketid,'server message');}
 function prntBlocked(args,socketid){return resultify(users.getUser(socketid).printBlocked(),true,socketid,'server message');}
-function block(args, socketid){//finish this
+
+function block(args, socketid){
 	var me = users.getUser(socketid);
 	var unblocked = [];
 	for (let a of args){
 		var jerk = users.isUser(a);
 		if(jerk !== false && jerk !== me){
-			if(me.isBlocked(jerk) === false){//test, add console.logz if still not work
+			if(me.isBlocked(jerk) === false){
 				me.block(jerk);
 			}else
 				unblocked.push(jerk.name+"(already on your blocked list)");
@@ -111,9 +126,10 @@ function block(args, socketid){//finish this
 	if(unblocked.length === 0)
 		return resultify("blocked "+ args.join(', '),true,socketid,'server message');
 	return resultify("Unable to block: " + unblocked.join(', ') ,false,socketid,'server message');
-
 }
-function removeFriend(args,socketid){//make this, add comands for this and above
+
+//remove a friend
+function removeFriend(args,socketid){
 	var me = users.getUser(socketid);
 	var unremoved = [];
 	for (let a of args){
@@ -147,6 +163,7 @@ function validName(name){
 	}
 	return true;
 }
+//format data into a result
 function resultify(msg,res,dest,type,data){
 	data = {};
 	data.msg = msg;
--- cmds/users.js
+++ cmds/users.js
@@ -1,5 +1,6 @@
 var users = [];
 
+/* -----  Exports  -----*/
 exports.online = function (){return users.length;}
 exports.isUser = function (name){
 	if(name.length === 0)
@@ -10,38 +11,51 @@ exports.isUser = function (name){
 				return u;
 			}
 	}
-	return false;}
+	return false;
+}
+
+//return the user for a given socket id
 exports.getUser =  function(socketid){
 	return getUsr(socketid);
 }
+
+//create a new user, connected on the given socket
 exports.newUsr = function(socketid){
 	var newuser = new user(socketid);
 	users.push(newuser);
 	return newuser.giveData;
 }
+
+//update a users information with the given info
 exports.updateUser = function(socketid,info){
 	u = getUsr(socketid).update(info)
 	console.log(u);
 	return u;
 }
+
+//delete the user account of someone who logged off
 exports.delSess = function(socketid){
 	var u = getUsr(socketid);
-	if(u.name !== null){
-		console.log(u.name,"... is gone sir");	
-	}
+	if(u.name !== null)
+		console.log(u.name,"logged off");	
 	u = users.indexOf(u);
 	users.splice(u,1);
-	return true;}
+	return true;
+}
+
+//get user helper function
 function getUsr(id){
 	for(var i = 0; i<users.length; i++){
 		var u = users[i];
 		if(u.id === id)
 			return u;
 	}
-	console.log("no luck");
-	return false;}
+	return false;
+}
 
+//user class
 var user = function(id){
+
 	this.sessId;
 	this.name = null;
 	this.id = id;
@@ -51,47 +65,60 @@ var user = function(id){
 	this.messages = {};
 	this.blocked = [];
 
+	//send a notificatoin to the user
 	this.notify = function(type,senderid,data){
+		//if user is blocked, dont notify
 		if(this.isBlocked(getUsr(senderid).name) === true)
 			return 0;
+		//activate new notification flag
 		this.notif[0] = true;
+
+		//format notification and add to notifications
 		if(type === 'Friend Request'){
 			if(this.isFriend(getUsr(senderid))===false)
 				this.notif.push([type,getUsr(senderid).name]);
 			else
 				this.notif.push([type,getUsr(senderid).name+" (added you back)"]);
-		}else if(type === 'Private Message'){
+		}else if(type === 'Private Message')
 			this.notif.push([type,getUsr(senderid).name,data]);
-		}else if(type === 'Block'){
+		else if(type === 'Block')
 			this.notif.push([type,senderid]);
-		}
+		
 	}
+
+	//supply the data for this user
 	this.giveData = function(){
 		return {name:this.name,id:this.id};
 	}
+
+	//update data and return new data
 	this.update = function(info){
 		if(info.name != undefined)
 			this.name = info.name;
 		return this.giveData;
 	}
+
+	//add a friend to friend list, unless blocked
 	this.addFriend = function(user){
 		if(this.isBlocked(user.name) === false)
 			this.friends.push(user);
 	}
+
+	//remove a friend from friend list
 	this.removeFriend = function(user){
 		var i = this.friends.indexOf(user);
 		this.friends.splice(i,1);
 		console.log("removed",user.name,"now, ",this.friends);
 		return true;
 	}
+
 	this.isFriend = function(user){
-		for(let f of this.friends){
-			if(f === user){
-				return true;
-			}
-		}
+		for(let f of this.friends)
+			if(f === user)return true;
 		return false;
 	}
+
+	//if there is new info, return it
 	this.newData = function(){
 		if(this.notif[0] === false)
 			return false;
@@ -101,18 +128,24 @@ var user = function(id){
 		this.notif = [false];
 		return info;
 	}
+
+	//return formated list of friends names
 	this.printFriends = function(){
 		var msg = '';
 		for(let f of this.friends)
 			msg+= f.name+ '\n ';
 		return msg;
 	}
+
+	//same as above but for blocked list
 	this.printBlocked = function(){
 		var msg = '';
 		for(let b of this.blocked)
 			msg+= b.name+ '\n ';
 		return msg;
 	}
+	
+	//block a user by name
 	this.block = function(user){
 		if(this.isFriend(user))
 			this.removeFriend(user)
@@ -121,11 +154,10 @@ var user = function(id){
 		this.notify("Block",user.id);
 		return true;
 	}
+	
 	this.isBlocked = function(user){
-		for(let b of this.blocked){
-			if(b === user)
-				return true;
-		}
+		for(let b of this.blocked)
+			if(b === user)return true;
 		return false;
 	}
 }
