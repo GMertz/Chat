7655e3d9da355a67b0262a42149261c78ad2335c
Gabe Mertz <gabe.mertz@crms.org>
2018-11-15 10:20:05 +0800

    changed port to 8080

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000,function(){
-	console.log('listening on *:3000');//make http server listen on port 3000
+http.listen(8080,function(){
+	console.log('listening on :8080');//make http server listen on port 3000
 });
